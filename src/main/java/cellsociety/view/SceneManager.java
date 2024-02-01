05711ef1ede529ecc66d0e0be7667e8a28d77264
Alisha Zhang <xz352@duke.edu>
2024-02-01 16:50:11 -0500

    test merge

--- src/main/java/cellsociety/view/SceneManager.java
+++ src/main/java/cellsociety/view/SceneManager.java
@@ -1,4 +1,21 @@
 package cellsociety.view;
 
+import javafx.scene.Scene;
+//import javafx.scene.text.Text;
+
+
 public class SceneManager {
+    private MenuPage selectSimulationPage;
+
+    static final double DISPLAY_WIDTH = 700;
+    static final double DISPLAY_HEIGHT = 500;
+
+    public SceneManager() {
+        selectSimulationPage = new MenuPage(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+    }
+
+    public Scene getMenuPage(){
+        return selectSimulationPage.getMenu();
+    }
+
 }
