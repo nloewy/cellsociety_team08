b21d38d43918bb7f9d0ad8d51e7d84104a64d227
Alisha Zhang <xz352@duke.edu>
2024-02-01 03:24:43 -0500

    CELL-18 and CELL-19: added new simulation button, about message button, and title text

--- src/main/java/cellsociety/view/SimulationPage.java
+++ src/main/java/cellsociety/view/SimulationPage.java
@@ -3,6 +3,8 @@ package cellsociety.view;
 
 import cellsociety.model.core.Cell;
 import cellsociety.model.core.Grid;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,6 +12,7 @@ import javafx.scene.layout.GridPane;
 
 import java.util.Iterator;
 import java.util.ResourceBundle;
+import javafx.scene.text.Text;
 
 
 public class SimulationPage {
@@ -17,36 +20,45 @@ public class SimulationPage {
     private GridPane grid;
     private Scene scene;
     private Group root;
+    private CellView[][] board;
+    private Button newSimulationButton;
+    private Button simulationInfoButton;
+    private Text simulationTitleDisplay;
 
-    Button backButton;
-    Button newSimulationButton;
     public static final String DEFAULT_RESOURCE_PACKAGE = "cellsociety.";
+    public static final int GRID_X_OFFSET = 100;
+    public static final int GRID_Y_OFFSET = 100;
 
-//Iterator<Cell> gridModelIterator
-    public SimulationPage(String simulationName, int numRows, int numCols, int cellWidth, int cellHeight) {
+    public SimulationPage(String simulationName, int numRows, int numCols, int cellWidth, int cellHeight, EventHandler<ActionEvent> newSimulationHandler, EventHandler<ActionEvent> infoButtonHandler) {
 //        buttonLables = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "buttonLables");
         root = new Group();
         grid = new GridPane();
+        scene = new Scene(root,700,700);
 
-        CellView[][] board = new CellView[numRows][numCols];
+        board = new CellView[numRows][numCols];
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j ++){
-                board[i][j] = new CellView(j*cellWidth, i*cellHeight);
-                grid.add(board[i][j].getCellGraphic(), j, i);
+                board[i][j] = new CellView(i, j);
+                grid.add(board[i][j].getCellGraphic(), i, j);
             }
         }
 
-        scene = new Scene(root,700,700);
-        root.getChildren().add(grid);
+        newSimulationButton = new Button("NewSimulation");
+        newSimulationButton.setOnAction(newSimulationHandler);
+        newSimulationButton.setLayoutY(100);
 
-//        backButton = new Button(buttonLables.getString("BackButton"));
-//        newSimulationButton = new Button(buttonLables.getString("NewSimulation"));
+        simulationInfoButton = new Button("About");
+        simulationInfoButton.setOnAction(infoButtonHandler);
+
+        simulationTitleDisplay = new Text(simulationName);
+
+        root.getChildren().addAll(grid, newSimulationButton, simulationInfoButton, simulationTitleDisplay);
     }
 
     public GridPane getGrid() {
         return grid;
     }
-    
+
     public Scene getSimulationScene(){
         return scene;
     }
@@ -57,6 +69,12 @@ public class SimulationPage {
 //            cell.updateStates();
 //        }
 //    }
+
+    public void updateView(Iterator gridIterator){
+        if (gridIterator.hasNext()){
+            gridIterator.next();
+        }
+    }
 
 
 }
