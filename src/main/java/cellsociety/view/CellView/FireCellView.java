9e1712e3bdd7e405f8b8428fc6f58c779a6bac2a
Noah Loewy <noah.loewy@duke.edu>
2024-02-05 15:37:07 +0000

    Update FireCellView.java

--- src/main/java/cellsociety/view/CellView/FireCellView.java
+++ src/main/java/cellsociety/view/CellView/FireCellView.java
@@ -1,10 +1,18 @@
 package cellsociety.view.CellView;
 
+/**
+ * the view component of cells in the spread of fire simulation
+ */
 public class FireCellView extends CellView {
 
   public FireCellView(int state, double width, double height) {
     super(state, width, height);
   }
+
+  /**
+   * sets the color of the view component of the cell according to their current state
+   * @param state an integer that specifies which state the cell is in
+   */
 
   @Override
   public void setColors(int state) {
