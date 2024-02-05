98bf4eadd376186487fcfccd475452f30a96d863
Noah Loewy <noah.loewy@duke.edu>
2024-02-05 14:12:56 +0000

    Revert "DESIGN-05"
    This reverts commit b45e0cf6211444f783779cef48a1ac1e8e7f4c9a

--- src/main/java/cellsociety/model/simulation/WatorSimulation.java
+++ src/main/java/cellsociety/model/simulation/WatorSimulation.java
@@ -175,10 +175,11 @@ public class WatorSimulation extends Simulation<WatorCell> {
       if (nextCell.getNextState() == SHARK || nextCell.getNextState() == FISH) {
         increaseFishAge(currentCell);
       } else {
-        if (currentCell.getAge() < fishAgeOfReproduction) {
-          nextCell.updateStateEnergyAge(FISH, -1, currentCell.getAge() + 1);
+        if(currentCell.getAge()<fishAgeOfReproduction){
+          nextCell.updateStateEnergyAge(FISH, -1, currentCell.getAge()+1);
           fillEmptyCell(currentCell);
-        } else {
+        }
+        else {
           reproduceFish(currentCell, nextCell);
         }
       }
@@ -238,8 +239,6 @@ public class WatorSimulation extends Simulation<WatorCell> {
               updateShark(currentCell);
               break;
             }
-            default:
-              break;
           }
         }
       }
