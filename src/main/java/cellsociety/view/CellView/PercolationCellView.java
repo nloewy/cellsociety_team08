3f068ffe13ac0600358b99398962805988bf9bb1
Alisha Zhang <xz352@duke.edu>
2024-02-04 23:20:27 -0500

    added javadoc comments

--- src/main/java/cellsociety/view/CellView/PercolationCellView.java
+++ src/main/java/cellsociety/view/CellView/PercolationCellView.java
@@ -1,11 +1,18 @@
 package cellsociety.view.CellView;
 
+/**
+ * the view component of cells in the percolation simulation
+ */
 public class PercolationCellView extends CellView {
 
   public PercolationCellView(int state, double width, double height) {
     super(state, width, height);
   }
 
+  /**
+   * sets the color of the view component of the cell according to their current state
+   * @param state an integer that specifies which state the cell is in
+   */
   @Override
   public void setColors(int state) {
     switch (state) {
