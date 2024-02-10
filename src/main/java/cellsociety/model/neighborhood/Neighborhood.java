8c7095c8cd45563f372bf38090487a9edf5531f8
Noah Loewy <noah.loewy@duke.edu>
2024-02-10 13:44:38 -0500

    SCHELLING BUG FIX

--- src/main/java/cellsociety/model/neighborhood/Neighborhood.java
+++ src/main/java/cellsociety/model/neighborhood/Neighborhood.java
@@ -7,19 +7,21 @@ import java.util.Iterator;
 import java.util.List;
 
 
+/**
+ * Class that represents a cell's neighborhood, which is a collection of points surrounding a center
+ * point.
+ *
+ * @author Noah Loewy
+ */
 
 public abstract class Neighborhood<T extends Cell> {
-  /**
-   * Class that represents a cell's neighborhood, which is a collection of points surrounding a
-   * center point.
-   *
-   * @author Noah Loewy
-   */
+
 
   /**
    * Retrieves all neighbors of a cell object using the 2D grid provided
-   * @param grid  the representation of the cells on the 2D plane
-   * @param cell  the central cell whose neighbors we are aiming to get
+   *
+   * @param grid the representation of the cells on the 2D plane
+   * @param cell the central cell whose neighbors we are aiming to get
    * @return a list of cells, each cell representing a neighbor of the given cell
    */
   public List<T> getNeighbors(Grid grid, T cell) {
@@ -40,9 +42,10 @@ public abstract class Neighborhood<T extends Cell> {
   /**
    * Abstract method that determines if two cells are neighbors under a certain grid. The
    * implementation of this method is dependent on the type of neighborhood being used.
-   * @param cell1  potential neighbor of cell 2
-   * @param cell2  potential neighbor of cell 1
-   * @param grid   grid object for the simulation
+   *
+   * @param cell1 potential neighbor of cell 2
+   * @param cell2 potential neighbor of cell 1
+   * @param grid  grid object for the simulation
    * @return true if and only if cell1 and cell2 are neighbors
    */
   public abstract boolean isValidNeighbor(T cell1, T cell2, Grid grid);
--- src/main/java/cellsociety/model/simulation/PercolationSimulation.java
+++ src/main/java/cellsociety/model/simulation/PercolationSimulation.java
@@ -30,7 +30,9 @@ public class PercolationSimulation extends Simulation<PercolationCell> {
    * @param row,       the number of rows in the 2-dimensional grid
    * @param col,       the number of columns in the 2-dimensional grid
    * @param hoodType,  the definition of neighbors
-   * @param stateList, a list of the integer representation of each cells state, by rows, then cols
+   * @param stateList, a list of the integer representation of each cells state in row major order
+   * @param r,         a record of all parameters needed for Percolation Simulation. Description of
+   *                   parameters can be found in the PercolationCell class
    */
   public PercolationSimulation(int row, int col, Neighborhood hoodType, List<Integer> stateList,
       PercolationRecord r) {
@@ -38,6 +40,14 @@ public class PercolationSimulation extends Simulation<PercolationCell> {
     this.percolatedNeighbors = r.percolatedNeighbors();
     createCellsAndGrid(row, col, stateList, getCellShape(r.cellShape()), hoodType);
   }
+
+  /**
+   * Creates list of PercolationCell objects to be passed into grid
+   * @param col number of columns in grid for simulation
+   * @param stateList list of all cell's states in row major order
+   * @param shape Shape object representing the shape of the cell as represented on 2d plane
+   * @return list of initialized PercolationCells
+   */
 
   public List<PercolationCell> cellMaker(int col, List<Integer> stateList, Shape shape) {
     List<PercolationCell> cellList = new ArrayList<>();
