16fa1fc0929b85cada20d554f689094bb0464234
Alisha Zhang <xz352@duke.edu>
2024-02-12 19:03:12 -0500

    DESIGN-07: javadoc for cellview, DESIGN-10: deleted unused stuff in button properties file

--- src/main/java/cellsociety/view/cellview/CellView.java
+++ src/main/java/cellsociety/view/cellview/CellView.java
@@ -10,6 +10,9 @@ import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Shape;
 import javafx.scene.shape.StrokeType;
 
+/**
+ * This class is the view component of a cell
+ */
 
 public abstract class CellView {
 
@@ -19,6 +22,13 @@ public abstract class CellView {
   private int boxLocationX;
   private int boxLocationY;
 
+  /**
+   * Constructs a cell view object
+   * @param width double, width of a cell
+   * @param height double, height of a cell
+   * @param vertices List, x,y coordinates of the vertices of the cell shape
+   * @param gridProperties
+   */
   public CellView(double width, double height, List<Point> vertices,
       Map<String, Double> gridProperties) {
     shape = new Polygon();
@@ -40,6 +50,10 @@ public abstract class CellView {
     shape.setStrokeType(StrokeType.OUTSIDE);
   }
 
+  /**
+   * This method turns the cell outline on and off according to the boolean status flag argument
+   * @param onOff boolean flag - whether the cell outline should go on or go off
+   */
   public void toggleStrokeWidth(boolean onOff) {
     if (onOff) {
       shape.setStrokeWidth(2);
@@ -48,27 +62,52 @@ public abstract class CellView {
     }
   }
 
+  /**
+   * abstract method, implemented by sub classes to set colors for cells depending on their state
+   * @param state int, the state the cell is in
+   */
   public abstract void setColors(int state);
 
+  /**
+   * Update cell status and color according to its new state
+   * @param state int, the new state the cell is going into
+   * @param text String, the text set onto the cell
+   */
   public void updateState(int state, String text) {
     getCellGraphic().getStyleClass().clear();
     textBox.setText(text);
     setColors(state);
   }
 
+  /**
+   * gets the css styling for the cell according to its id
+   * @param idName String, css id of the cell
+   */
   public void getCss(String idName) {
     shape.setId(idName);
     shape.getStyleClass().add(idName);
   }
 
+  /**
+   * gets the cell shape graphic
+   * @return returns the shape object of the cell
+   */
   public Shape getCellGraphic() {
     return shape;
   }
 
+  /**
+   * gets the label of the cell
+   * @return textbox object
+   */
   public Label getTextBox() {
     return textBox;
   }
 
+  /**
+   * gets the x coordinate of the textbox
+   * @return double, the x coordinate
+   */
   public double getBoxLocationX() {
 
     Bounds textBounds = textBox.getLayoutBounds();
@@ -76,6 +115,10 @@ public abstract class CellView {
     return boxLocationX - textBoxWidth;
   }
 
+  /**
+   * gets the y coordniate of the textbox
+   * @return double, the y coordinate.
+   */
   public double getBoxLocationY() {
     Bounds textBounds = textBox.getLayoutBounds();
     double textBoxHeight = textBounds.getHeight();
